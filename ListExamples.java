a991feb41e2c418b9551bbcf2c3948efe537af1d
Steven Tang <146782397+steventang5@users.noreply.github.com>
2023-11-27 15:01:24 -0800

    Update ListExamples.java
    Added bugs to ListExamples, infinite loop

--- ListExamples.java
+++ ListExamples.java
@@ -15,6 +15,7 @@ class ListExamples {
         result.add(s);
       }
     }
+
     return result;
   }
 
@@ -24,19 +25,22 @@ class ListExamples {
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
+    while(true){
+        System.out.println(1);
+    }
     while(index1 < list1.size() && index2 < list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
         result.add(list1.get(index1));
-        index1 += 1;
+        index1 += 2;
       }
       else {
         result.add(list2.get(index2));
-        index2 += 1;
+        index2 += 5;
       }
     }
     while(index1 < list1.size()) {
       result.add(list1.get(index1));
-      index1 += 1;
+      index1 += 10;
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
